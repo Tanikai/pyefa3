2aee396fb391810b4e77bfda06b4b247efbd8a76
Kai Anter <kai@anter.dev>
2024-07-08 22:45:53 +0200

    chore: formatting

--- pyefa/networks.py
+++ pyefa/networks.py
@@ -4,14 +4,17 @@ import efa, exceptions
 
 supported = ('vrr', 'vrn')
 
+
 class VRR(efa.EFA):
-	efa_triprequest_url = 'http://efa.vrr.de/vrr/XSLT_TRIP_REQUEST2'
+    efa_triprequest_url = 'http://efa.vrr.de/vrr/XSLT_TRIP_REQUEST2'
+
 
 class VRN(efa.EFA):
-	efa_triprequest_url = 'http://fahrplanauskunft.vrn.de/vrn/XSLT_TRIP_REQUEST2'
-	
+    efa_triprequest_url = 'http://fahrplanauskunft.vrn.de/vrn/XSLT_TRIP_REQUEST2'
+
+
 def network(network):
-	global supported
-	if network not in supported:
-		return None
-	return globals()[network.upper()]()
+    global supported
+    if network not in supported:
+        return None
+    return globals()[network.upper()]()
